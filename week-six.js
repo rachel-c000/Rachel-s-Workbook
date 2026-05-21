4a1536194fa7ad07b7fc57846bf3c0466bf512ae
rachel-c000 <rachelchen789@gmail.com>
2026-05-21 23:48:41 +1000

    week six files

--- week-six.js
+++ week-six.js
@@ -17,3 +17,21 @@
       if (!isLifted) folder.classList.add('lifted');
     });
   });
+
+  // for the spinner!
+
+let rotation = 0;
+let spinning = false;
+let stopTimer;
+
+document.addEventListener('mousemove', () => {
+  rotation += 8;
+  document.getElementById('spinner').style.transform = `rotate(${rotation}deg)`;
+
+  if (!spinning) spinning = true;
+
+  clearTimeout(stopTimer);
+  stopTimer = setTimeout(() => {
+    spinning = false;
+  }, 100);
+});
